e9e51751e3883e93cae473b33984d69ddb3b57ff
laura Mesa <lauramesa71@gmail.com>
2023-05-18 11:55:44 -0500

    boton resetear

---
+++ public/script/mensajeAccion.js
@@ -0,0 +1,34 @@
+/*
+const encriptar1 = document.getElementById("1");
+const desencriptar2 = document.getElementById("2");
+const copiar3 = document.getElementById("3");
+const cerrar = document.getElementById("4");
+
+encriptar1.addEventListener("click", identificarBoton);
+desencriptar2.addEventListener("click", identificarBoton);
+copiar3.addEventListener("click", identificarBoton);
+cerrar.addEventListener("click", identificarBoton);
+
+var fondo = document.querySelector('.show_background');
+var mensaje = document.querySelector('.show');
+
+
+function alerta() {
+  mostrarMensaje();
+}
+
+function mostrarMensaje() {
+  fondo.style.display = "block";
+  mensaje.style.display = "block";
+}
+
+function ocultarMensaje() {
+  fondo.style.display = "none";
+  mensaje.style.display = "none";
+}
+
+function identificarBoton(event) {
+  return event.target.id;
+}
+
+*/
---
+++ public/script/resetear.js
@@ -0,0 +1,7 @@
+function resetear(){
+    var textArea = document.querySelector('.incoming__text');
+    var parrafo = document.querySelector('.textResult');
+
+    textArea.value = "";
+    parrafo.innerHTML = "Ningun Mensaje fue encontrado..."
+}
